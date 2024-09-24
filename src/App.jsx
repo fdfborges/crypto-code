a14f4b5f9f48f6f43a1a69ceaae6b5f5169a32f9
Felipe de Freitas Borges <felipedefreitasborges@gmail.com>
2024-09-23 22:15:07 -0300

    Part of the pricing completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Companies from "./components/companies/Companies";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Pricing from "./components/pricing/Pricing";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   <Navbar />
   <Hero />
   <Companies />
+  <Pricing />
     </>
   )
 }
